de8acb16aba921e26fb534b6260460f0fc5f0265
JaydenHuang8 <jah059@ucsd.edu>
2026-05-18 13:01:26 -0700

    restructure, and added dvc, marimo

---
+++ notebooks/report.py
@@ -0,0 +1,22 @@
+import marimo
+
+__generated_with = "0.23.6"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
